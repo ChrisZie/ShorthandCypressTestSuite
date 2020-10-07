989b12f2e9e6f623a0411146560e6d397954cbec
ChrisZie <chrissie2788@gmail.com>
2020-10-07 22:42:17 +0800

    Add comments and increase timeout time

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,26 +1,30 @@
 //reusable command to login into shorthand
 Cypress.Commands.add('login', () =>{
   cy.visit('/signin')
+  //Fill in email address and password
   cy.get('#identifier')
     .type('christine.zierold@gmail.com')
   cy.get('#password')
     .type('Test12345')
+  //submit the form
   cy.get("form").submit()
   //Check the right path to the dashboard
   cy.location('pathname', {timeout: 10000})
     .should('include', 'organisations');
   //Add longer timeout because of current loading problems
   //Check if dashboard is loaded --> login successfull
-  cy.get('div.dashboard-container', {timeout: 10000})
-    .should('be.visible', {timeout: 10000})
+  cy.get('div.dashboard-container', {timeout: 20000})
+    .should('be.visible', {timeout: 20000})
 })
 
 Cypress.Commands.add('createBlankStory', () =>{
-  //ToDo: create a better identifier for this button e.g. Data-QA attribute
+  // Click on "New Story"
+  //ToDo: create a better selector for this button e.g. Data-QA attribute
   cy.get('.card-new-story button.card-link')
     .should('be.visible')
     .click()
-  //ToDo: create a better identifier for this button e.g. Data-QA attribute
+  //Click/Select blank template in appearing pop-up window
+  //ToDo: create a better selector for this button e.g. Data-QA attribute
   cy.get('div.card-template-blank-container button.card-link')
     .should('be.visible')
     .click()
